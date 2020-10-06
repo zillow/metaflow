aec769307851622297e83def6ef6ede62da2b865
Hari Sezhiyan <hariharans@zg06955.zillow.local>
2020-10-06 16:17:49 -0700

    Getting rid of unused imports and cleaning grammar.

--- metaflow/plugins/kfp/tests/run_integration_tests.py
+++ metaflow/plugins/kfp/tests/run_integration_tests.py
@@ -1,12 +1,8 @@
-import argparse
 from os import listdir
 from os.path import isfile, join
 from subprocess import run, PIPE
 
 import kfp
-
-from metaflow.util import get_username
-from metaflow.metaflow_config import KFP_SDK_API_NAMESPACE, KFP_RUN_URL_PREFIX
 
 import pytest
 
@@ -19,8 +15,8 @@ each kfp run, waits for the run to fully complete, and prints whether
 or not the run was successful. It also checks to make sure the logging
 functionality works.
 
-More specifically, the tests spawn KFP runs, and ensure the spawning processes
-returns a returncode of 0. However, the logs of the run within KFP are simply logged
+More specifically, the tests spawn KFP runs and ensure the spawning processes
+have a returncode of 0. However, the logs of the runs within KFP are simply logged
 to the screen, and it is the responsibility of the user to read these logs to ensure
 the run executed correctly on KFP.
 
@@ -31,14 +27,14 @@ Parameters:
 
 
 def parse_run_id(output):
-	start_idx = output.find("run_id|")
-	end_idx = output.find("|end_id")
+    start_idx = output.find("run_id|")
+    end_idx = output.find("|end_id")
 
-	if start_idx == -1 or end_idx == -1:
-		return -1
+    if start_idx == -1 or end_idx == -1:
+        return -1
 
-	run_id = output[start_idx + len("run_id|"):end_idx]
-	return run_id
+    run_id = output[start_idx + len("run_id|") : end_idx]
+    return run_id
 
 
 def obtain_flow_file_paths(flow_dir_path):
@@ -47,8 +43,7 @@ def obtain_flow_file_paths(flow_dir_path):
 
 
 @pytest.mark.parametrize(
-    'flow_file_path',
-    obtain_flow_file_paths("metaflow/plugins/kfp/tests/sample_flows")
+    "flow_file_path", obtain_flow_file_paths("metaflow/plugins/kfp/tests/sample_flows")
 )
 def test_sample_flows(flow_file_path):
     full_path = join("metaflow/plugins/kfp/tests/sample_flows", flow_file_path)
@@ -56,18 +51,19 @@ def test_sample_flows(flow_file_path):
         f"python3 {full_path} --datastore=s3 kfp run --wait-for-completion",
         text=True,
         stdout=PIPE,
-        shell=True
-    )   
+        shell=True,
+    )
     assert run_and_wait_process.returncode == 0
-    
+
     # we check for the correct logging of only the 'start' and 'end'
-    # steps because these are the only steps gauranteed by Metaflow
+    # steps because these are the only steps gauranteed to exist
+    # in a Metaflow flow
     run_id = parse_run_id(run_and_wait_process.stdout)
     check_valid_logs_process = run(
         f"python3 {full_path} --datastore=s3 logs kfp-{run_id}/start &&"
         f"python3 {full_path} --datastore=s3 logs kfp-{run_id}/end",
         text=True,
-        shell=True
+        shell=True,
     )
     assert check_valid_logs_process.returncode == 0
 
