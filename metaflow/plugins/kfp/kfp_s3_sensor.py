e72adacee6cebe23e4fae5846ec46d15ecc287df
Hariharan Sezhiyan <hsezhiyan@ucdavis.edu>
2021-07-01 11:50:07 -0700

    Add output annotation.

--- metaflow/plugins/kfp/kfp_s3_sensor.py
+++ metaflow/plugins/kfp/kfp_s3_sensor.py
@@ -1,5 +1,6 @@
-def wait_for_s3_path(path: str, timeout: int) -> str:
+def wait_for_s3_path(path: str, timeout: int) -> None:
     print("path: ", path)
+    print("timeout: ", timeout)
     import time
     print("Waiting for sleep to end...")
     time.sleep(60)
