5a4e20971d9074ddda7486088761819836964281
Hariharan Sezhiyan <hsezhiyan@ucdavis.edu>
2021-07-13 22:43:39 -0700

    Adding more comprehensive unit tests.

--- metaflow/plugins/kfp/tests/test_kfp_s3_sensor.py
+++ metaflow/plugins/kfp/tests/test_kfp_s3_sensor.py
@@ -1,3 +1,4 @@
+from unittest import mock
 from metaflow.plugins.kfp.kfp_s3_sensor import wait_for_s3_path
 
 from unittest.mock import call, Mock, patch, PropertyMock
@@ -5,9 +6,14 @@ import pytest
 
 import boto3
 from botocore.exceptions import ClientError
+from moto import mock_s3
+
+import tempfile
 
 import base64
 import marshal
+
+import os
 
 """
 To run these tests from your terminal, go to the root directory and run:
@@ -22,6 +28,68 @@ for the integration tests.
 
 def identity_formatter(path: str, flow_parameters: dict) -> str:
     return path
+
+
+@mock_s3
+@pytest.mark.parametrize(
+    "upload_bucket, upload_key, upload_path, processed_path, flow_parameters_json, os_vars",
+    [
+        (
+            "sample_bucket",
+            "sample_prefix/sample_file.txt",
+            "s3://sample_bucket/sample_prefix/sample_file.txt",
+            "s3://sample_bucket/sample_prefix/sample_file.txt",
+            '{"key": "value"}',
+            False,
+        ),
+        (
+            "sample_bucket",
+            "sample_prefix/date=07-02-2021/sample.txt",
+            "s3://sample_bucket/sample_prefix/date={date}/sample.txt",
+            "s3://sample_bucket/sample_prefix/date=07-02-2021/sample.txt",
+            '{"date": "07-02-2021"}',
+            False,
+        ),
+        (
+            "sample_bucket",
+            "sample_prefix/date=08-03-2022/sample.txt",
+            "s3://sample_bucket/sample_prefix/date=$DATE/sample.txt",
+            "s3://sample_bucket/sample_prefix/date=08-03-2022/sample.txt",
+            '{"key": "value"}',
+            True,
+        ),
+    ],
+)
+def test_wait_for_s3_path_no_exception(
+    upload_bucket: str,
+    upload_key: str,
+    upload_path: str,
+    processed_path: str,
+    flow_parameters_json: str,
+    os_vars: bool,
+):
+    os.environ["DATE"] = "08-03-2022"
+
+    identity_formatter_code_encoded = base64.b64encode(
+        marshal.dumps(identity_formatter.__code__)
+    ).decode("ascii")
+
+    upload_file = tempfile.NamedTemporaryFile()
+
+    s3 = boto3.resource("s3", region_name="us-east-1")
+    s3.create_bucket(Bucket=upload_bucket)
+    s3.meta.client.upload_file(upload_file.name, upload_bucket, upload_key)
+
+    path = wait_for_s3_path(
+        path=upload_path,
+        timeout_seconds=1,
+        polling_interval_seconds=1,
+        path_formatter_code_encoded=identity_formatter_code_encoded,
+        flow_parameters_json=flow_parameters_json,
+        os_vars=os_vars,
+    )
+
+    assert path == processed_path
 
 
 # This test ensures a timeout exception is raised when wait_for_s3_path
@@ -39,4 +107,5 @@ def test_wait_for_s3_path_timeout_exception():
             polling_interval_seconds=1,
             path_formatter_code_encoded=identity_formatter_code_encoded,
             flow_parameters_json='{"key": "value"}',
+            os_vars=False,
         )
