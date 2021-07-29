fcb4ecf036832a5101ab6324d07f814d0e90eb3e
Yun Wu <wuyun1618@gmail.com>
2021-07-29 00:28:50 -0700

    Fix env var
    Fix typo in test

--- metaflow/plugins/kfp/kfp_decorator.py
+++ metaflow/plugins/kfp/kfp_decorator.py
@@ -84,6 +84,10 @@ class KfpInternalDecorator(StepDecorator):
         self.datastore = datastore
         self.logger = logger
 
+        env_deco = [deco for deco in graph[step].decorators if deco.name == "environment"]
+        if env_deco:
+            os.environ.update(env_deco[0].attributes["vars"].items())
+
     def task_pre_step(
         self,
         step_name,
