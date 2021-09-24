4258da67e969267df0d56d2e223208d0083998e0
Hari Sezhiyan <hariharans@zillowgroup.com>
2021-09-24 15:51:53 -0700

    Integration tests should now work.

--- metaflow/plugins/kfp/tests/flows/kfp_step_flow.py
+++ metaflow/plugins/kfp/tests/flows/kfp_step_flow.py
@@ -3,7 +3,27 @@ import os
 
 from kubernetes import client, config
 
-kfp_step_image_1 = "analytics-docker.artifactory.zgtools.net/artificial-intelligence/ai-platform/aip-py39-cpu:0.1.1402"
+kfp_step_image_1 = "analytics-docker.artifactory.zgtools.net/artificial-intelligence/ai-platform/aip-py39-cpu:0.1.1410"
+kfp_step_image_2 = "analytics-docker.artifactory.zgtools.net/artificial-intelligence/ai-platform/aip-py39-cpu:0.1.1412"
+
+
+def assert_step_image(kfp_step_image: str):
+    is_on_kubernetes = os.getenv("K8S_CLUSTER_NAME")
+    if is_on_kubernetes:  # only perform this test on the cluster, not on local machine
+        config.load_incluster_config()
+        core_api_instance = client.CoreV1Api()
+
+        current_pod_name = os.environ.get("HOSTNAME", None)
+        current_pod_namespace = os.environ.get("POD_NAMESPACE", None)
+
+        if current_pod_name and current_pod_namespace:
+            pod_detail = core_api_instance.read_namespaced_pod(
+                namespace=current_pod_namespace, name=current_pod_name
+            )
+
+            for container_status in pod_detail.status.container_statuses:
+                if container_status.name == "main":
+                    assert container_status.image == kfp_step_image
 
 
 class KfpStepFlow(FlowSpec):
@@ -11,35 +31,18 @@ class KfpStepFlow(FlowSpec):
     Test kfp_step(image=...)
     """
 
-    @kfp_step(image="analytics-docker.artifactory.zgtools.net/artificial-intelligence/ai-platform/aip-py39-cpu:0.1.1402")
+    @kfp_step(image=kfp_step_image_1)
     @step
     def start(self):
         print("Start step, testing for correct image.")
-        def assert_step_image(kfp_step_image: str):
-            is_on_kubernetes = os.getenv("K8S_CLUSTER_NAME")
-            if is_on_kubernetes:  # only perform this test on the cluster, not on local machine
-                config.load_incluster_config()
-                core_api_instance = client.CoreV1Api()
-
-                current_pod_name = os.environ.get("HOSTNAME", None)
-                current_pod_namespace = os.environ.get("POD_NAMESPACE", None)
-
-                if current_pod_name and current_pod_namespace:
-                    pod_detail = core_api_instance.read_namespaced_pod(
-                        namespace=current_pod_namespace, name=current_pod_name
-                    )
-
-                    for container_status in pod_detail.status.container_statuses:
-                        if container_status.name == "main":
-                            assert container_status.image == kfp_step_image
-        assert_step_image("analytics-docker.artifactory.zgtools.net/artificial-intelligence/ai-platform/aip-py39-cpu:0.1.1402")
-        
+        assert_step_image(kfp_step_image_1)
         self.next(self.end)
 
-    @kfp_step(image="analytics-docker.artifactory.zgtools.net/artificial-intelligence/ai-platform/aip-py39-cpu:0.1.1408")
+    @kfp_step(image=kfp_step_image_2)
     @step
     def end(self):
-        print("End step.")
+        print("End step, testing for correct image.")
+        assert_step_image(kfp_step_image_2)
 
 
 if __name__ == "__main__":
