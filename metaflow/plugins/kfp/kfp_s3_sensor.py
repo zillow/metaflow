4b0913b5849e2f249f9cf58bad52f563c78f418d
Hari Sezhiyan <hariharans@zillowgroup.com>
2021-12-01 14:45:21 -0800

    Black formatting issue.

--- metaflow/plugins/kfp/kfp_s3_sensor.py
+++ metaflow/plugins/kfp/kfp_s3_sensor.py
@@ -21,6 +21,7 @@ from typing import Tuple
 
 from metaflow.plugins.aws.aws_client import get_aws_client
 
+
 def construct_elapsed_time_s3_bucket_and_key(
     flow_name: str, kfp_run_id: str
 ) -> Tuple[str, str]:
