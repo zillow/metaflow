7d746e23f4bbdf63ecc90bbb539482a86f6a826f
chrisvan <chrisvan@zillowgroup.com>
2022-07-22 19:18:50 +0000

    added wait functionality, still with many print statements. Uses Metaflow layer to check if exception raised to determine if a flow failed. I would have preferred to check whether finished != successful (at task/step/run levels), but that did not work, as print statements in this version show

--- metaflow/plugins/argo/argo_inner_flow.py
+++ metaflow/plugins/argo/argo_inner_flow.py
@@ -31,7 +31,6 @@ def run_inner_flow(
     inner_namespace = flow_information['metadata']['namespace']
     finished = False
     successful = None
-    data = None
     
     output = {
         "inner_run_argo_id": inner_run_argo_id,
@@ -39,7 +38,7 @@ def run_inner_flow(
         "inner_namespace": inner_namespace,
         "finished": finished,
         "successful": successful,
-        "data": data,
+        "exception": None
     }
     
     if wait:
@@ -62,29 +61,42 @@ def run_inner_flow(
             print("Inner flow failed to begin running")
             return output
             
-        print(f"Inner flow has started: {inner_run}")
+        print(f"Inner flow has started: {inner_run}\nNow we will wait for the flow to finish")
         
-        print("Steps (starting):")
-        steps = set()
-        for step in inner_run.steps():    # what are tags???
-            print(step.)
-            if step not in steps:
-                steps.add(step)
-        
-        
-        print(f"steps: {steps}")
-        
-        ### CURRENT ISSUE IS I NEED TO CONVERT STEP TO A string SO I CAN KEEP TRACK OF IN SET. SEE DOCUMENTATION TO DIG DOWN.
-        ### Also, I should look outside the .client file b/c there may be more stuff in dev files???
-        
-
+        steps = []
         attempts_to_finish_run = 30  # in minutes (5s for testing)
         while attempts_to_finish_run > 0:
             if inner_run.finished:
                 break
             else:
-                if attempts_to_finish_run % 12 == 5:
-                    print("Inner flow still running, check back in one minute (5s for testing)")
+
+                # if attempts_to_finish_run % 12 == 5:
+                print("Inner flow still running, check back in one minute (5s for testing)")
+
+                for step in inner_run.steps():
+                    step_str = str(step)
+                    if step_str not in steps:
+                        print(f"Adding step: {step_str}")
+                        steps.append(step_str)
+
+                    for task in step.tasks():
+                        if task.exception is not None:
+                            output["finished"] = True
+                            output["successful"] = False
+                            output["exception"] = {"step": step, "task": task, "exception": task.exception}
+                            print(f"EXITING EARLY BC OF FAILURE @ TASK: {task}")
+                            print(f"type(task.exception): {type(task.exception)}")
+                            print(f"task.finished: {task.finished}")
+                            print(f"task.successful: {task.successful}")
+                            print("waiting a minute to confirm...")
+                            time.sleep(60)
+                            print(f"type(task.exception): {type(task.exception)}")
+                            print(f"task.finished: {task.finished}")
+                            print(f"task.successful: {task.successful}")
+                            print("ok, now exiting run flow function")
+                            return output
+                    
+                # sleep and decrement attempts remaining
                 time.sleep(5)
                 attempts_to_finish_run -= 1
         
@@ -92,16 +104,22 @@ def run_inner_flow(
         
         print("Steps (final):")
         for step in inner_run.steps():    # what are tags???
-            print(step)
-            if step not in steps:
+            step_str = str(step)
+            print(f"step_str: {step_str}")
+            print(f"finished at: {step.finished_at}")
+            if step_str not in steps:
                 print("adding this ^^^ step")
-                steps.add(step)
+                steps.append(step_str)
+            for task in step.tasks():
+                print(f"task: {task}")
+                if task.exception is not None:
+                    print(f"EXCEPTION: {task.exception}")
+        print("")
         
         print(f"steps: {steps}")
         
         output["finished"] = inner_run.finished
         output["successful"] = inner_run.successful
-        output["data"] = inner_run.data
         print("Inner flow is finished!!!")
     
     else:
