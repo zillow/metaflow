f4180b6c7bba3a547e0ddb120210d548379a6d2d
Chris Vandervoort <cavandervoort@gmail.com>
2022-08-18 14:36:34 -0700

    cleaned up test file

--- metaflow/plugins/argo/test_run.py
+++ metaflow/plugins/argo/test_run.py
@@ -8,29 +8,15 @@ run = trigger_live_run(
     flow_name=flow_name,
     alt_flow_id_info={'template_name': template_name},
     parameters=None,
-    wait=True,
+    wait=False,
 )
 
 print(f"\nRunning flow {run.flow_name}")
 start_time = time.time()
 while run.is_running:
-    print(f"""
-In progress status after {int((time.time()-start_time)//1)} seconds:
-    Status (private): {run._plugin_run._status}
-    Has Triggered:    {run.has_triggered}
-    Is Running:       {run.is_running}
-    Successful:       {run.successful}
-    Failed Steps:     {run.failed_steps}
-    Exceptions:       {run.exceptions}\n""")
-    time.sleep(15)
+    print(f"Status (private): '{run._plugin_run._status}' after {int((time.time()-start_time)//1)} seconds")
+    run._plugin_run._print_status()
+    time.sleep(15 - (time.time() - start_time) % 15)
 
-print(f"""
-Final status after {int((time.time()-start_time)//1)} seconds:
-    Status (private): {run._plugin_run._status}
-    Has Triggered:    {run.has_triggered}
-    Is Running:       {run.is_running}
-    Successful:       {run.successful}
-    Failed Steps:     {run.failed_steps}
-    Exceptions:       {run.exceptions}
-""")
-
+print(f"Final status (private): {run._plugin_run._status} after {int((time.time()-start_time)//1)} seconds")
+run._plugin_run._print_status()
