488d67e6787fcf23848612127ea710fc3461fffc
Alex Latchford <alexla@zillowgroup.com>
2022-09-15 16:48:34 -0700

    AIP-6643: Ensure we continue to capture stdout and stderr logs

--- metaflow/plugins/kfp/tests/conftest.py
+++ metaflow/plugins/kfp/tests/conftest.py
@@ -1,5 +1,12 @@
-import pytest
+import logging
+import sys
 from pathlib import Path
+from io import TextIOBase
+import pytest
+
+
+# Use the root logger for stdout/stderr patching.
+logger = logging.getLogger()
 
 
 def pytest_addoption(parser):
@@ -12,6 +19,22 @@ def pytest_addoption(parser):
         "--opsgenie-api-token", dest="opsgenie_api_token", action="store", default=None
     )
     parser.addoption("--public-dir", dest="public_dir", action="store", default='')
+
+
+class StreamToLogger(TextIOBase):
+    """Fake file-like stream object that redirects writes to a logger instance."""
+
+    def __init__(self, logger, level):
+        self.logger = logger
+        self.level = level
+        self.linebuf = ""
+
+    def write(self, buf):
+        for line in buf.rstrip().splitlines():
+            self.logger.log(self.level, line.rstrip())
+
+    def flush(self):
+        pass
 
 
 @pytest.hookimpl(hookwrapper=True, tryfirst=True)
@@ -27,4 +50,8 @@ def pytest_runtest_setup(item):
         f"{item._request.node.name}.log"
     )
     logging_plugin.set_log_path(str(filename))
+
+    # Forward logs from stdout/stderr to the logger as well.
+    sys.stdout = StreamToLogger(logger, logging.INFO)
+    sys.stderr =  StreamToLogger(logger, logging.ERROR)
     yield
