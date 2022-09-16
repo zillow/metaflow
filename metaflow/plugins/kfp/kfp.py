c86e782cb27cc6817ae7a652a1f09c54de735916
aaronare <aaronare@zillowgroup.com>
2022-09-16 16:54:42 -0700

    add in env var for SA

--- metaflow/plugins/kfp/kfp.py
+++ metaflow/plugins/kfp/kfp.py
@@ -695,6 +695,7 @@ class KubeflowPipelines(object):
                     "ZODIAC_SERVICE": "metadata.labels['zodiac.zillowgroup.net/service']",
                     "ZODIAC_TEAM": "metadata.labels['zodiac.zillowgroup.net/team']",
                     "ZODIAC_OWNER": "metadata.labels['zodiac.zillowgroup.net/owner']",
+                    "METAFLOW_KUBERNETES_SERVICE_ACCOUNT": KUBERNETES_SERVICE_ACCOUNT,
                 }
                 for name, resource in env_vars.items():
                     op.container.add_env_variable(
