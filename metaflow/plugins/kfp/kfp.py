f4e15f669d755c19bb54642b99db099c1b3e6218
aaronare <aaronare@zillowgroup.com>
2022-09-16 17:07:00 -0700

    lint

--- metaflow/plugins/kfp/kfp.py
+++ metaflow/plugins/kfp/kfp.py
@@ -696,9 +696,11 @@ class KubeflowPipelines(object):
                     "ZODIAC_TEAM": "metadata.labels['zodiac.zillowgroup.net/team']",
                     "ZODIAC_OWNER": "metadata.labels['zodiac.zillowgroup.net/owner']",
                 }
-                # add in spark env variable or ServiceAccount 
+                # add in spark env variable or ServiceAccount
                 if KUBERNETES_SERVICE_ACCOUNT:
-                    env_vars["METAFLOW_KUBERNETES_SERVICE_ACCOUNT"] = KUBERNETES_SERVICE_ACCOUNT
+                    env_vars[
+                        "METAFLOW_KUBERNETES_SERVICE_ACCOUNT"
+                    ] = KUBERNETES_SERVICE_ACCOUNT
 
                 for name, resource in env_vars.items():
                     op.container.add_env_variable(
