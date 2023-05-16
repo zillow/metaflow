d69258508e2d656d19e12954eda34c0709077ba5
Taleb Zeghmi <talebz@zillowgroup.com>
2023-05-16 12:35:14 -0700

    AIP-7314 Argo Flow Trigger Flow

--- metaflow/plugins/kfp/kfp_cli.py
+++ metaflow/plugins/kfp/kfp_cli.py
@@ -1,4 +1,5 @@
 import json
+import re
 import shutil
 import subprocess
 from typing import Dict, Any
@@ -18,6 +19,12 @@ from metaflow.package import MetaflowPackage
 from metaflow.plugins.aws.step_functions.step_functions_cli import (
     check_metadata_service_version,
 )
+from metaflow.plugins.kfp.argo_utils import (
+    run_id_to_url,
+    run_id_to_metaflow_url,
+    to_metaflow_run_id,
+)
+from metaflow.plugins.kfp.kfp_decorator import KfpException
 from metaflow.plugins.kfp.kfp_step_init import save_step_environment_variables
 from metaflow.util import get_username
 
@@ -56,7 +63,7 @@ def step_init(obj, run_id, step_name, passed_in_split_indexes, task_id):
 
 
 @parameters.add_custom_parameters(deploy_mode=True)
-@kubeflow_pipelines.command(help="Submit this flow to Argo.")
+@kubeflow_pipelines.command(help="Submit this flow to run in the cluster.")
 @click.option(
     "--experiment",
     "-e",
@@ -85,9 +92,9 @@ def step_init(obj, run_id, step_name, passed_in_split_indexes, task_id):
 )
 @click.option(
     "--namespace",
-    "namespace",
+    "user_namespace",
     default=None,
-    help="Metaflow read namespace.",
+    help="Change the Metaflow namespace from the default to the given Metaflow namespace.",
     show_default=True,
 )
 @click.option(
@@ -203,7 +210,7 @@ def run(
     experiment=None,
     tags=None,
     sys_tags=None,
-    namespace=None,
+    user_namespace=None,
     kubernetes_namespace=KUBERNETES_NAMESPACE,
     yaml_only=False,
     yaml_format=None,
@@ -246,7 +253,7 @@ def run(
         tags=tags,
         sys_tags=sys_tags,
         experiment=experiment,
-        namespace=namespace,
+        user_namespace=user_namespace,
         base_image=base_image,
         s3_code_package=s3_code_package,
         max_parallelism=max_parallelism,
@@ -260,7 +267,7 @@ def run(
         if pipeline_path is None:
             raise CommandException("Please specify --pipeline-path")
 
-        pipeline_path = flow.create_workflow_yaml_file(
+        workflow, pipeline_path = flow.create_workflow_yaml_file(
             pipeline_path, flow_parameters, yaml_format
         )
         obj.echo(f"\nDone compiling *{current.flow_name}* to {pipeline_path}")
@@ -278,9 +285,9 @@ def run(
             kubernetes_namespace, flow_parameters
         )
         argo_run_id = workflow_manifest["metadata"]["name"]
-        metaflow_run_id = f"argo-{argo_run_id}"
-        metaflow_ui_url = f"{METAFLOW_RUN_URL_PREFIX}/{flow.name}/{metaflow_run_id}"
-        argo_ui_url = f"{ARGO_RUN_URL_PREFIX}/argo-ui/workflows/{kubernetes_namespace}/{argo_run_id}"
+        metaflow_run_id = to_metaflow_run_id(argo_run_id)
+        metaflow_ui_url = run_id_to_metaflow_url(flow.name, argo_run_id)
+        argo_ui_url = run_id_to_url(argo_run_id, kubernetes_namespace)
         # ddog_ui_url = (
         #     f"https://ai-platform.datadoghq.com/orchestration/overview/pod?query=annotation%23metaflow.org%2F"
         #     f"run_id%3A{metaflow_run_id}"
@@ -327,6 +334,314 @@ def run(
             )
 
 
+@kubeflow_pipelines.command(help="Deploy a new version of this flow to the cluster.")
+@click.option(
+    "--name",
+    "name",
+    default=None,
+    help="The workflow template name. The default is the flow name.",
+    show_default=True,
+)
+@click.option(
+    "--experiment",
+    "-e",
+    "experiment",
+    default=None,
+    help="The associated experiment name for the run. ",
+    show_default=True,
+)
+@click.option(
+    "--tag",
+    "tags",
+    multiple=True,
+    default=None,
+    help="Annotate all objects produced by Argo Workflows runs "
+    "with the given tag. You can specify this option multiple "
+    "times to attach multiple tags.",
+)
+@click.option(
+    "--sys-tag",
+    "sys_tags",
+    multiple=True,
+    default=None,
+    help="Annotate all Metaflow objects produced by Argo Metaflow runs "
+    "with the given system tag. You can specify this option multiple "
+    "times to attach multiple tags.",
+)
+@click.option(
+    "--namespace",
+    "user_namespace",
+    default=None,
+    help="Change the namespace from the default (production token) "
+    "to the given tag. See run --help for more information.",
+)
+@click.option(
+    "--k8s-namespace",
+    "--kubernetes-namespace",
+    "kubernetes_namespace",
+    default=KUBERNETES_NAMESPACE,
+    help="Kubernetes Namespace for your run in Argo.",
+    show_default=True,
+)
+@click.option(
+    "--yaml-only",
+    "yaml_only",
+    is_flag=True,
+    default=False,
+    help="Generate the Workflow YAML which is used to run the workflow on Argo.",
+    show_default=True,
+)
+@click.option(
+    "--pipeline-path",
+    "pipeline_path",
+    default=None,
+    help="The output path of the generated Argo pipeline yaml file",
+    show_default=False,
+)
+@click.option(
+    "--s3-code-package/--no-s3-code-package",
+    "s3_code_package",
+    default=True,
+    help="Whether to package the code to S3 datastore",
+    show_default=True,
+)
+@click.option(
+    "--base-image",
+    "base_image",
+    default=KFP_DEFAULT_CONTAINER_IMAGE,
+    help="Base docker image used in Argo.",
+    show_default=True,
+)
+@click.option(
+    "--max-parallelism",
+    "-m",
+    default=KFP_MAX_PARALLELISM,
+    show_default=True,
+    help="Maximum number of parallel pods.",
+)
+@click.option(
+    "--workflow-timeout", default=None, type=int, help="Workflow timeout in seconds."
+)
+@click.option(
+    "--notify",
+    "-n",
+    "notify",
+    is_flag=True,
+    default=bool(from_conf("METAFLOW_NOTIFY")),
+    help="Whether to notify upon completion.  Default is METAFLOW_NOTIFY env variable. "
+    "METAFLOW_NOTIFY_ON_SUCCESS and METAFLOW_NOTIFY_ON_ERROR env variables determine "
+    "whether a notification is sent.",
+    show_default=True,
+)
+@click.option(
+    "--notify-on-error",
+    "-noe",
+    "notify_on_error",
+    default=from_conf("METAFLOW_NOTIFY_ON_ERROR", default=None),
+    help="Email address to notify upon error. "
+    "If not set, METAFLOW_NOTIFY_ON_ERROR is used from Metaflow config or environment variable",
+    show_default=True,
+)
+@click.option(
+    "--notify-on-success",
+    "-nos",
+    "notify_on_success",
+    default=from_conf("METAFLOW_NOTIFY_ON_SUCCESS", default=None),
+    help="Email address to notify upon success"
+    "If not set, METAFLOW_NOTIFY_ON_SUCCESS is used from Metaflow config or environment variable",
+    show_default=True,
+)
+@click.pass_obj
+def create(
+    obj,
+    name=None,
+    experiment=None,
+    tags=None,
+    sys_tags=None,
+    user_namespace=None,
+    kubernetes_namespace=KUBERNETES_NAMESPACE,
+    yaml_only=False,
+    pipeline_path=None,
+    s3_code_package=True,
+    base_image=None,
+    max_parallelism=None,
+    workflow_timeout=None,
+    notify=False,
+    notify_on_error=None,
+    notify_on_success=None,
+):
+    obj.check(obj.graph, obj.flow, obj.environment, pylint=obj.pylint)
+
+    check_metadata_service_version(obj)
+    flow = make_flow(
+        obj=obj,
+        name=obj.flow.name,
+        tags=tags,
+        sys_tags=sys_tags,
+        experiment=experiment,
+        user_namespace=user_namespace,
+        base_image=base_image,
+        s3_code_package=s3_code_package,
+        max_parallelism=max_parallelism,
+        workflow_timeout=workflow_timeout,
+        notify=notify,
+        notify_on_error=notify_on_error,
+        notify_on_success=notify_on_success,
+    )
+
+    workflow_name: str = resolve_workflow_name(name)
+    if yaml_only:
+        if pipeline_path is None:
+            raise CommandException("Please specify --pipeline-path")
+
+        workflow, path = flow.create_workflow_yaml_file(
+            pipeline_path, output_format="argo-workflow-template", name=workflow_name
+        )
+        obj.echo(
+            f"\nDone compiling *{flow.name}* to {path} with {workflow['metadata']['name']}"
+        )
+    else:
+        obj.echo(f"Deploying *{flow.name}* to Argo Workflows...", bold=True)
+        workflow_template: Dict[str, Any] = flow.deploy(
+            kubernetes_namespace, workflow_name
+        )
+        template_name = workflow_template["metadata"]["name"]
+        obj.echo(
+            f"Workflow Template *{template_name}* "
+            f"for flow *{current.flow_name}* pushed to "
+            "Argo Workflows successfully.\n",
+            bold=True,
+        )
+        argo_ui_url = f"{ARGO_RUN_URL_PREFIX}/argo-ui/workflow-templates/{kubernetes_namespace}/{template_name}"
+        obj.echo(f"*Argo:* {argo_ui_url}", fg="cyan")
+
+        # TODO(talebz): using @schedule decorator?
+        #  flow.schedule()
+        #  obj.echo("What will trigger execution of the workflow:", bold=True)
+        #  obj.echo(flow.trigger_explanation(), indent=True)
+
+
+@parameters.add_custom_parameters(deploy_mode=False)
+@kubeflow_pipelines.command(help="Trigger the workflow on Argo Workflows.")
+@click.option(
+    "--name",
+    "name",
+    default=None,
+    help="The workflow template name. The default is the flow name.",
+    show_default=True,
+)
+@click.option(
+    "--k8s-namespace",
+    "--kubernetes-namespace",
+    "kubernetes_namespace",
+    default=KUBERNETES_NAMESPACE,
+    help="Kubernetes Namespace for your run in Argo.",
+    show_default=True,
+)
+@click.option(
+    "--run-id-file",
+    default=None,
+    show_default=True,
+    type=str,
+    help="Write the ID of this run to the file specified.",
+)
+@click.pass_obj
+def trigger(
+    obj,
+    name=None,
+    kubernetes_namespace=KUBERNETES_NAMESPACE,
+    run_id_file=None,
+    **kwargs,
+):
+    def _convert_value(param):
+        # Swap `-` with `_` in parameter name to match click's behavior
+        val = kwargs.get(param.name.replace("-", "_").lower())
+        if param.kwargs.get("type") == JSONType:
+            val = json.dumps(val)
+        return val
+
+    params = {
+        param.name: _convert_value(param)
+        for _, param in obj.flow._get_parameters()
+        if kwargs.get(param.name.replace("-", "_").lower()) is not None
+    }
+
+    # def _convert_value(param: parameters.Parameter):
+    #     v = kwargs.get(param.name)
+    #     return json.dumps(v) if param.kwargs.get("type") == JSONType else v
+    #
+    # flow_parameters = {
+    #     param.name: _convert_value(param)
+    #     for _, param in obj.flow._get_parameters()
+    #     if kwargs.get(param.name) is not None
+    # }
+
+    from metaflow.plugins.kfp.kfp import KubeflowPipelines
+
+    workflow_name: str = resolve_workflow_name(name)
+    workflow: Dict[str, Any] = KubeflowPipelines.trigger(
+        kubernetes_namespace, workflow_name, params
+    )
+    argo_run_id = workflow["metadata"]["name"]
+    metaflow_run_id = to_metaflow_run_id(argo_run_id)
+
+    if run_id_file:
+        with open(run_id_file, "w") as f:
+            f.write(str(argo_run_id))
+
+    obj.echo(
+        f"Workflow *{workflow_name}* triggered on Argo Workflows "
+        f"(Argo run-id *{argo_run_id}*).",
+        bold=True,
+    )
+
+    metaflow_ui_url = run_id_to_metaflow_url(current.flow_name, argo_run_id)
+    argo_ui_url = run_id_to_url(argo_run_id, kubernetes_namespace)
+
+    obj.echo(f"Metaflow run_id=*{metaflow_run_id}*\n", fg="magenta")
+    obj.echo(f"*Argo:* {argo_ui_url}", fg="cyan")
+    obj.echo(f"*Metaflow:* {metaflow_ui_url}", fg="cyan")
+
+    argo_workflow_name = workflow["metadata"]["name"]
+
+    obj.echo(
+        f"*Argo workflow:* argo -n {kubernetes_namespace} watch {argo_workflow_name}\n",
+        fg="cyan",
+    )
+
+
+def resolve_workflow_name(name: str) -> str:
+    valid_name = re.compile("^[a-z0-9]([a-z0-9\.\-]*[a-z0-9])?$")
+    if name and not valid_name.search(name):
+        raise MetaflowException(
+            f"Name '{name}' contains invalid characters. The "
+            "name must consist of lower case alphanumeric characters, '-' or '.'"
+            ", and must start and end with an alphanumeric character."
+        )
+
+    workflow_name = name if name else current.flow_name
+
+    if len(workflow_name) > 253:
+        msg = (
+            f"The full name of the workflow:\n*{workflow_name}*\n"
+            "is longer than 253 characters.\n\n"
+            "To deploy this workflow to Argo Workflows, please assign a shorter name\n"
+            "using the option\n"
+            "*kfp --name <name> create*." % workflow_name
+        )
+        raise KfpException(msg)
+
+    if not valid_name.search(workflow_name):
+        workflow_name = (
+            re.compile(r"^[^A-Za-z0-9]+")
+            .sub("", workflow_name)
+            .replace("_", "")
+            .lower()
+        )
+
+    return workflow_name
+
+
 def show_status(
     run_id: str, argo_ui_url: str, metaflow_ui_url: str, echo: callable, succeeded: bool
 ):
@@ -345,7 +660,7 @@ def make_flow(
     tags,
     sys_tags,
     experiment,
-    namespace,
+    user_namespace,
     base_image,
     s3_code_package,
     max_parallelism,
@@ -400,7 +715,7 @@ def make_flow(
         tags=tags,
         sys_tags=sys_tags,
         experiment=experiment,
-        namespace=namespace,
+        user_namespace=user_namespace,
         username=get_username(),
         max_parallelism=max_parallelism,
         workflow_timeout=workflow_timeout,
