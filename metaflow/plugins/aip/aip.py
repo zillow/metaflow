bcc1cd1798cf1b504bbccbf55b7b39eb0936d982
Taleb Zeghmi <talebz@zillowgroup.com>
2023-10-10 13:00:01 -0700

    AIP-7726 add default cards

--- metaflow/plugins/aip/aip.py
+++ metaflow/plugins/aip/aip.py
@@ -286,7 +286,22 @@ class KubeflowPipelines(object):
         for key, value in self._get_flow_labels().items():
             workflow["metadata"]["labels"][key] = value
 
+        KubeflowPipelines._add_archive_section_to_cards_artifacts(workflow)
+
         return workflow
+
+    @staticmethod
+    def _add_archive_section_to_cards_artifacts(workflow: dict):
+        print("in _add_archive_section_to_cards_artifacts")
+        # for any "-cards" artifacts add "archive" section with "none" value
+        for template in workflow["spec"]["templates"]:
+            print("first template")
+            if "outputs" in template and "artifacts" in template["outputs"]:
+                for artifact in template["outputs"]["artifacts"]:
+                    print(f"artifact: {artifact}")
+                    if "-cards" in artifact["name"]:
+                        print(f"adding archive none")
+                        artifact["archive"] = {"none": {}}
 
     @staticmethod
     def _config_map(workflow_name: str, max_run_concurrency: int):
@@ -1364,7 +1379,9 @@ class KubeflowPipelines(object):
             None if node.name == "start" else {"flow_parameters_json": "None"}
         )
 
-        file_outputs: Dict[str, str] = {}
+        file_outputs: Dict[str, str] = {
+            "cards_default": "/tmp/outputs/cards/default_card.html",
+        }
         if node.type == "foreach":
             file_outputs["foreach_splits"] = "/tmp/outputs/foreach_splits/data"
         for preceding_component_input in preceding_component_inputs:
