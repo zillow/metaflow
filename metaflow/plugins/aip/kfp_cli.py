e2e593747645e9a7a34c71acf28687b6257ccfd2
Taleb Zeghmi <talebz@zillowgroup.com>
2023-10-13 15:20:59 -0700

    PR Feedback

--- metaflow/plugins/aip/kfp_cli.py
+++ metaflow/plugins/aip/kfp_cli.py
@@ -1,3 +1,6 @@
+import inspect
+from pathlib import Path
+
 from metaflow._vendor import click
 from metaflow.plugins.aip.aip_decorator import AIPException
 
@@ -14,5 +17,31 @@ def kubeflow_pipelines(obj):
 
 
 @kubeflow_pipelines.command(help="Deprecated, please use aip instead.")
-def run():
-    raise AIPException('Deprecated, please use "aip run" instead.')
+@click.pass_obj
+def run(
+    obj,
+):
+    file_name = Path(inspect.getfile(obj.flow.__class__)).name
+    raise AIPException(f'Deprecated, please use "python {file_name} aip run" instead.')
+
+
+@kubeflow_pipelines.command(help="Deprecated, please use aip instead.")
+@click.pass_obj
+def create(
+    obj,
+):
+    file_name = Path(inspect.getfile(obj.flow.__class__)).name
+    raise AIPException(
+        f'Deprecated, please use "python {file_name} aip create" instead.'
+    )
+
+
+@kubeflow_pipelines.command(help="Deprecated, please use aip instead.")
+@click.pass_obj
+def trigger(
+    obj,
+):
+    file_name = Path(inspect.getfile(obj.flow.__class__)).name
+    raise AIPException(
+        f'Deprecated, please use "python {file_name} aip trigger" instead.'
+    )
