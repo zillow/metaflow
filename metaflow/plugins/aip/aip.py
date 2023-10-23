e2f1f3e173e88923ccba0dc8ba03f2bbcfceed18
Taleb Zeghmi <talebz@zillowgroup.com>
2023-10-22 22:25:48 -0700

    AIP-7784 validate MF tags w/ k8s label rules - AIP-7393 Add Flow labels to Workflow for ArgoUI #259 https://github.com/zillow/metaflow/pull/259 introduced this bug where AIP flow tags become k8s labels and hence need the same k8s label value validation.

--- metaflow/plugins/aip/aip.py
+++ metaflow/plugins/aip/aip.py
@@ -4,6 +4,7 @@ import json
 import marshal
 import numbers
 import os
+import re
 import sys
 from dataclasses import dataclass
 from pathlib import Path
@@ -900,6 +901,24 @@ class KubeflowPipelines(object):
                 raise ValueError(
                     f"Tag name {annotation_name} must be no more than 63 characters"
                 )
+            if len(annotation_value) > 63:
+                raise ValueError(
+                    f"Tag value {annotation_value} must be no more than 63 characters"
+                )
+
+            pattern = r"(([A-Za-z0-9][-A-Za-z0-9_.]*)?[A-Za-z0-9])?"
+            regex_match = re.fullmatch(pattern, annotation_value)
+            if not regex_match:
+                raise ValueError(
+                    f"Tag {annotation_name} value {annotation_value} must must"
+                    "consist of alphanumeric "
+                    "characters, '-', '_' or '.', and "
+                    "must start and end with an alphanumeric "
+                    "character. "
+                    "Example 'MyValue',  or 'my_value',  or '12345', regex used "
+                    "for validation is '(([A-Za-z0-9][-A-Za-z0-9_.]*)?[A-Za-z0-9])?'"
+                )
+
             ret_flow_labels[annotation_name] = annotation_value
 
         # - In context of Zillow CICD self.username == "cicd_compile"
