ec6d3d1be4d0a993c8c52f8a9a7020853af16ab1
Taleb Zeghmi <talebz@zillowgroup.com>
2023-10-24 11:41:57 -0700

    add unit tests

--- metaflow/plugins/aip/aip.py
+++ metaflow/plugins/aip/aip.py
@@ -284,7 +284,9 @@ class KubeflowPipelines(object):
             }
 
         # add Flow labels as Workflow labels to be searchable in the Argo UI
-        for key, value in self._get_flow_labels().items():
+        for key, value in KubeflowPipelines._get_flow_labels(
+            self.name, self.experiment, self.tags, self.sys_tags, self.username
+        ).items():
             workflow["metadata"]["labels"][key] = value
 
         KubeflowPipelines._add_archive_section_to_cards_artifacts(workflow)
@@ -876,18 +878,25 @@ class KubeflowPipelines(object):
         )
         return (resource, volume)
 
-    def _get_flow_labels(self) -> Dict[str, str]:
+    @staticmethod
+    def _get_flow_labels(
+        flow_name: str,
+        experiment: Optional[str],
+        tags: List[str],
+        sys_tags: List[str],
+        username: str,
+    ) -> Dict[str, str]:
         # function return variable
         ret_flow_labels: Dict[str, str] = {}
 
         prefix = "metaflow.org"
-        ret_flow_labels[f"{prefix}/flow_name"] = self.name
-        if self.experiment:
-            ret_flow_labels[f"{prefix}/experiment"] = self.experiment
+        ret_flow_labels[f"{prefix}/flow_name"] = flow_name
+        if experiment:
+            ret_flow_labels[f"{prefix}/experiment"] = experiment
 
         all_tags = list()
-        all_tags += self.tags if self.tags else []
-        all_tags += self.sys_tags if self.sys_tags else []
+        all_tags += tags if tags else []
+        all_tags += sys_tags if sys_tags else []
         for tag in all_tags:
             if ":" in tag:  # Metaflow commonly uses <name>:<value> as tag format
                 tag_info = tag.split(":", 1)
@@ -910,21 +919,19 @@ class KubeflowPipelines(object):
             regex_match = re.fullmatch(pattern, annotation_value)
             if not regex_match:
                 raise ValueError(
-                    f"Tag {annotation_name} value {annotation_value} must must"
-                    "consist of alphanumeric "
-                    "characters, '-', '_' or '.', and "
-                    "must start and end with an alphanumeric "
-                    "character. "
-                    "Example 'MyValue',  or 'my_value',  or '12345', regex used "
-                    "for validation is '(([A-Za-z0-9][-A-Za-z0-9_.]*)?[A-Za-z0-9])?'"
+                    f"Tag {annotation_name} value {annotation_value} must "
+                    "consist of alphanumeric characters, '-', '_' or '.', and "
+                    "must start and end with an alphanumeric character.  Example "
+                    "'MyValue',  or 'my_value',  or '12345', regex used for "
+                    "validation is '(([A-Za-z0-9][-A-Za-z0-9_.]*)?[A-Za-z0-9])?'"
                 )
 
             ret_flow_labels[annotation_name] = annotation_value
 
-        # - In context of Zillow CICD self.username == "cicd_compile"
-        # - In the context of a Zillow NB self.username == METAFLOW_USER (user_alias)
-        # - In the context of Metaflow integration tests self.username == USER=$GITLAB_USER_EMAIL
-        owner = self.username
+        # - In context of Zillow CICD username == "cicd_compile"
+        # - In the context of a Zillow NB username == METAFLOW_USER (user_alias)
+        # - In the context of Metaflow integration tests username == USER=$GITLAB_USER_EMAIL
+        owner = username
         if "@" in owner:
             owner = owner.split("@")[0]
         ret_flow_labels["zodiac.zillowgroup.net/owner"] = owner
@@ -948,7 +955,9 @@ class KubeflowPipelines(object):
         container_op.add_pod_label("sidecar.istio.io/inject", "false")
 
         # add Flow labels as container labels
-        for key, value in self._get_flow_labels().items():
+        for key, value in KubeflowPipelines._get_flow_labels(
+            self.name, self.experiment, self.tags, self.sys_tags, self.username
+        ).items():
             container_op.add_pod_label(key, value)
 
         prefix = "metaflow.org"
--- metaflow/plugins/aip/tests/run_integration_tests.py
+++ metaflow/plugins/aip/tests/run_integration_tests.py
@@ -15,6 +15,7 @@ from subprocess_tee import run
 
 from . import _python, obtain_flow_file_paths
 from metaflow.exception import MetaflowException
+from ..aip import KubeflowPipelines
 
 """
 To run these tests from your terminal, go to the tests directory and run: 
@@ -200,28 +201,28 @@ def test_error_and_opsgenie_alert(pytestconfig) -> None:
     return
 
 
-@pytest.mark.parametrize(
-    "flow_file_path",
-    obtain_flow_file_paths("flows", non_standard_test_flows + disabled_test_flows),
-)
-def test_flows(pytestconfig, flow_file_path: str) -> None:
-    full_path: str = os.path.join("flows", flow_file_path)
-
-    test_cmd: str = (
-        f"{_python()} {full_path} --datastore=s3 --with retry aip run "
-        f"--argo-wait --workflow-timeout 1800 "
-        f"--max-parallelism 3 --experiment metaflow_test --tag test_t1 "
-        f"--sys-tag test_sys_t1:sys_tag_value "
-        f"--tag {pytestconfig.getoption('pipeline_tag')} "
-    )
-    if pytestconfig.getoption("image"):
-        test_cmd += (
-            f"--no-s3-code-package --base-image {pytestconfig.getoption('image')}"
-        )
-
-    run_cmd_with_backoff_from_platform_errors(test_cmd, correct_return_code=0)
-
-    return
+# @pytest.mark.parametrize(
+#     "flow_file_path",
+#     obtain_flow_file_paths("flows", non_standard_test_flows + disabled_test_flows),
+# )
+# def test_flows(pytestconfig, flow_file_path: str) -> None:
+#     full_path: str = os.path.join("flows", flow_file_path)
+#
+#     test_cmd: str = (
+#         f"{_python()} {full_path} --datastore=s3 --with retry aip run "
+#         f"--argo-wait --workflow-timeout 1800 "
+#         f"--max-parallelism 3 --experiment metaflow_test --tag test_t1 "
+#         f"--sys-tag test_sys_t1:sys_tag_value "
+#         f"--tag {pytestconfig.getoption('pipeline_tag')} "
+#     )
+#     if pytestconfig.getoption("image"):
+#         test_cmd += (
+#             f"--no-s3-code-package --base-image {pytestconfig.getoption('image')}"
+#         )
+#
+#     run_cmd_with_backoff_from_platform_errors(test_cmd, correct_return_code=0)
+#
+#     return
 
 
 def run_cmd_with_backoff_from_platform_errors(
@@ -426,3 +427,30 @@ def test_toleration_and_affinity_compile_only(pytestconfig) -> None:
         key="node.k8s.zgtools.net/purpose",
         value="high-memory",
     )
+
+
+def test_flow_labels():
+    tags: Dict[str, str] = KubeflowPipelines._get_flow_labels(
+        flow_name="my_flow",
+        experiment="my_experiment",
+        tags=["t1"],
+        sys_tags=["s1"],
+        username="foo",
+    )
+
+    assert tags["metaflow.org/flow_name"] == "my_flow"
+    assert tags["metaflow.org/experiment"] == "my_experiment"
+    assert tags["metaflow.org/tag_t1"] == "true"
+    assert tags["metaflow.org/tag_s1"] == "true"
+    assert tags["zodiac.zillowgroup.net/owner"] == "foo"
+
+    with pytest.raises(ValueError) as e:
+        KubeflowPipelines._get_flow_labels(
+            flow_name="my_flow",
+            experiment="my_experiment",
+            tags=["t1"],
+            sys_tags=["s1:a/b"],
+            username="foo",
+        )
+
+    assert "a/b must consist of alphanumeric characters" in str(e.value)
