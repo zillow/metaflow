4018c1484fde0289f2dfa1aebe1bf5ec228a419d
v-abhijeets <v-abhijeets@zillowgroup.com>
2026-07-06 20:52:34 +0530

    AIP-10272 : Update Argo URL  to match recent Argo UI changes

--- metaflow/plugins/aip/argo_utils.py
+++ metaflow/plugins/aip/argo_utils.py
@@ -344,7 +344,7 @@ def get_argo_url(
     kubernetes_namespace: str,
     argo_workflow_uid: str,
 ):
-    argo_ui_url = f"{ARGO_RUN_URL_PREFIX}/argo-ui/workflows/{kubernetes_namespace}/{argo_run_id}?uid={argo_workflow_uid}"
+    argo_ui_url = f"{ARGO_RUN_URL_PREFIX}/workflows/{kubernetes_namespace}/{argo_run_id}?uid={argo_workflow_uid}"
     return argo_ui_url
 
 
